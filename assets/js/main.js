e5282c45c4438b39c57966b12a5ab78ee46f83a9
Ivo Zinenda <izinenda@gmail.com>
2020-01-06 21:18:13 +0200

    FAB

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
-    $('.tooltipped').tooltip();
     $('.fixed-action-btn').floatingActionButton();
     $('.modal').modal();
-
-    var $container = $('#masonry-grid');
-    // initialize
-    $container.masonry({
-      columnWidth: '.col',
-      itemSelector: '.col',
-    });
 });
